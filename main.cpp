e4bf8396528fe6cc936d763fe24bac59955d71a1
ivanhernandezroldan <ivanhernandezroldan1@gmail.com>
2023-03-06 16:33:03 +0100

    debug testing

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
-int main(){
-    cout << "helo";
+int main(int argc, char const *argv[])
+{
+    int i= 0;
+    i++;
+    cout << i << endl;
+    cout << "Hello world" << endl;
+    return 0;
 }
